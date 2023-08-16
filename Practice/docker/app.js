cb541c451d56c31a4adb6e8e28f1742930644f70
thaint-zenkenplanet <thai.n.t@zenkenplanet-vn.com>
2023-08-16 09:04:42 +0700

    docker

---
+++ Practice/docker/app.js
@@ -0,0 +1,16 @@
+// app.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route to handle incoming requests
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
